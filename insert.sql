18be7cc133ea8974d5f5bd6b81b2bde9d7433d4e
Kate <katepdonahue@gmail.com>
2013-12-16 17:05:24 -0500

    Add quiz questions

---
+++ insert.sql
@@ -0,0 +1,34 @@
+INSERT INTO users(name)
+VALUE ("Armando");
+INSERT INTO users(name)
+VALUE ("Ariel");
+INSERT INTO users(name)
+VALUE ("Nisha");
+INSERT INTO quizzes (name)
+VALUE ("Getting to Know Us");
+INSERT INTO quizzes (name)
+VALUE ("Getting to Know Us -- REMIX");
+INSERT INTO questions (name, quiz_id)
+VALUE ("Which of the following is a group member's super hero name?", 1);
+INSERT INTO questions (name, quiz_id)
+VALUE ("What was Kate for Halloween this year?", 1);
+INSERT INTO questions (name, quiz_id)
+VALUE ("What does Armando say when a friend he hasn't heard from in awhile asks him for a favor?", 1);
+INSERT INTO questions (name, quiz_id)
+VALUE ("What's Nisha's favorite comeback?", 1);
+INSERT INTO questions (name, quiz_id)
+VALUE ("What's Ariel's least favorite vegetable?", 1);
+INSERT INTO questions (name, quiz_id)
+VALUE ("Which group member does NOT wash their arms?", 2);
+INSERT INTO questions (name, quiz_id)
+VALUE ("What were the ingredients of Armando's Leftover Jambalaya?", 2);
+INSERT INTO questions (name, quiz_id)
+VALUE ("What did Amine say about Nisha?", 2);
+INSERT INTO questions (name, quiz_id)
+VALUE ("What is the correct pronunciation of Ariel's name?", 2);
+INSERT INTO questions (name, quiz_id)
+VALUE ("Which group member was Prom King/Quenn?", 2);
+
+
+
+
